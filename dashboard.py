e139a80f7d98f8c6eca8065c271a049fdcd6212c
Daniel Olaya <d_olaya@hotmail.com>
2019-11-18 12:37:01 -0800

    Added dropbox selection for timeframe

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import pandas as pd
-from tools import get_price, update_loan_ratio
+from tools import get_price, update_loan_ratios
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -12,32 +12,50 @@ import plotly.graph_objs as go
 app = dash.Dash()
 
 try:
-    df = pd.read_csv('./data/cdp.csv')
+    df_cdp = pd.read_csv('./data/cdp.csv')
+    df_btcusd = pd.read_csv('./data/btcusd.csv')
 except FileNotFoundError:
-    print('ERROR - Loan data (cdp.csv) file not found, exiting program')
+    print('ERROR - File not found, exiting program')
     exit(1)
 
-df.set_index('num', inplace=True)
-print(df.head())
+df_cdp.set_index('num', inplace=True)
+df_btcusd['Date'] = pd.to_datetime(df_btcusd['Date'])
+df_btcusd['Last'] = pd.to_numeric(df_btcusd['Last'])
+print(df_btcusd.head())
 
 
-# curr_price = get_price()
 
 app.layout = html.Div([
+    # Price ticker
     html.H1(id='btc-price', children='BTC: USD.'),
     dcc.Interval(id='update_interval', interval=10000, n_intervals=0),
-    html.Div(children='what a steal')
+
+    # Time frame selection
+    html.Div([
+        html.Div([
+            html.Label(children='Select time frame:')
+        ], style={'width': '150px', 'display': 'inline-block', 'vertical-align': 'middle'}),
+        html.Div([
+            dcc.Dropdown(
+                options=[
+                    {'label': '90 days', 'value': 90},
+                    {'label': '60 days', 'value': 60},
+                    {'label': '30 days', 'value': 30}
+                ],
+                value=60)
+        ], style={'width': '150px', 'display': 'inline-block', 'vertical-align': 'top'})
+    ], style={'padding-top': '20px'})
+
 ])
 
 
 @app.callback(Output('btc-price', 'children'),
               [Input('update_interval', 'n_intervals')])
 def update_stats(n_intervals):
-    global df
-    print('\n updating stats...')
-    curr_price = get_price()
-    # update_loan_ratio(df, curr_price)
-    return 'BTC: '+str(curr_price)+' USD'
+    global df_cdp
+    price = get_price()
+    df_cdp['coll_ratio'] = update_loan_ratios(df_cdp, price)
+    return 'BTC: '+str(price['USD'])+' USD - '+str(price['CAD'])+' CAD'
 
 if __name__ == '__main__':
     app.run_server()
--- tools.py
+++ tools.py
@@ -2,18 +2,25 @@ import requests
 import json
 
 bitfinex_url = 'https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD'
+bankofcanada_url = 'https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?recent=1'
 
 def get_price():
+    price = {}
     response = requests.get(bitfinex_url)
     json_response = json.loads(response.text)
-    curr_price = json_response[0][1]
-    print('new price is:{}'.format(curr_price))
-    return curr_price
+    usd_price = json_response[0][1]
 
-def update_loan_ratio(df, curr_price):
+    response = requests.get(bankofcanada_url)
+    json_response = json.loads(response.text)
+    fx_rate = float(json_response['observations'][0]['FXCADUSD']['v'])
+    cad_price = round(usd_price / fx_rate, 1)
+
+    price.update({'USD': usd_price, 'CAD': cad_price})
+    return price
+
+def update_loan_ratios(df, price):
     ratio = []
-    for i in df.index:
-        print(type(i))
-        print(df.iloc('i'))
-        # print('i:{}', i['amount'])
-        break
+    for index, row in df.iterrows():
+        ratio.append((price['CAD'] * row['coll_amount'])/ row['loan_amount'])
+    return ratio
+
