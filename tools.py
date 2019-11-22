59044ffcfea3466e83323f9c657f25093e33d2d8
Daniel Olaya <d_olaya@hotmail.com>
2019-11-21 17:53:16 -0800

    Error handling for API calls

--- tools.py
+++ tools.py
@@ -1,13 +1,10 @@
 import requests
 import json
-
+import sys
 
 def get_price():
     price = {}
-    bitfinex_url = 'https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD'
-    response = requests.get(bitfinex_url)
-    json_response = json.loads(response.text)
-    usd_price = json_response[0][1]
+    usd_price = call_exchange_api()
 
     # cad_price = round(usd_price / fx_rate, 1)
     # price.update({'USD': usd_price, 'CAD': cad_price})
@@ -15,23 +12,36 @@ def get_price():
     return price
 
 
-def get_cadusd_rates(start_date):
-    bankofcanada_url = 'https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?start_date='+start_date
-    response = requests.get(bankofcanada_url)
-    json_response = json.loads(response.text)
-    # fx_rate = float(json_response['observations'][0]['FXCADUSD']['v'])
-    # print('\n\nget_cadusd_rate response')
-    # print(json_response)
-    # print()
-    # for item in json_response['observations']:
-    #     print(item['FXCADUSD']['v'])
-    fill_rates_weekend_data(json_response)
+def call_exchange_api():
+    bitfinex_url = 'https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD'
+    response = None
+    try:
+        response = requests.get(bitfinex_url)
+    except TimeoutError as ex:
+        print('Bitfinex API unavailable, try again later.', ex)
+        # TODO: show user message error before terminating program
+        sys.exit(1)
+    return json.loads(response.text)[0][1]
 
+
+def get_cadusd_rates(start_date):
+    json_response = call_fx_api(start_date)
+    fill_rates_weekend_data(json_response)
     return [i['FXCADUSD']['v'] for i in json_response['observations']]
-    # print(json_response['observations'])
+
+
+def call_fx_api(start_date):
+    bankofcanada_url = 'https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?start_date='+start_date
+    response = None
+    try:
+        response = requests.get(bankofcanada_url, timeout=1)
+    except TimeoutError as ex:
+        print("Bank of Canada API unavailable, try again later.", ex)
+        sys.exit(1)
+    return json.loads(response.text)
 
 def fill_rates_weekend_data(data):
+    # Bitcoin never closes its branch, so need to make up for banks missing data
     print(data['observations'])
     for i in data['observations']:
         print(i)
-        # TODO: complete this
