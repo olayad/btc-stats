65b8ecccbed7fd747d208a438175ddab90cfe9c1
Daniel Olaya <d_olaya@hotmail.com>
2019-11-23 15:07:12 -0800

    Tests completed (all pass) for fill_missing_day_rates

--- tools.py
+++ tools.py
@@ -8,6 +8,8 @@ quandl_url = 'https://www.quandl.com/api/v3/datasets/BITFINEX/BTCUSD.csv?api_key
 bitfinex_url = 'https://api-pub.bitfinex.com/v2/tickers?symbols=tBTCUSD'
 bankofcanada_url = 'https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?'
 
+# Update value every yearly average for CADUSD rate.
+AVG_FXCADUSD = 0.753    # Last updated Nov 23, 2019
 
 def update_btcusd_data():
     try:
@@ -26,7 +28,7 @@ def get_price():
 
 
 def call_exchange_api():
-    response = Noneg
+    response = None
     try:
         response = requests.get(bitfinex_url, timeout=2)
     except requests.exceptions.Timeout:
@@ -36,27 +38,15 @@ def call_exchange_api():
 
 
 def get_cadusd_rates(start_date, end_date=str(datetime.date.today())):
-    print('(get_cadusd_rates) - start:{},  end:{}'.format(start_date, end_date))
     json_response = call_fx_api(start_date, end_date)
-    observations = {i['d']: i['FXCADUSD']['v'] for i in json_response['observations']}
-    params = {'start_date': start_date, 'end_date': end_date}
-    rates = [params, observations]
-    print('rates(should be list with 2 dicts):', rates)
 
+    observations = {}
+    if payload_is_not_empty(json_response):
+        observations = strip_payload(json_response)
+    api_data = [{'start_date': start_date, 'end_date': end_date}, observations]
 
-    fill_missing_day_rates(rates)
-
-
-    return [i['FXCADUSD']['v'] for i in json_response['observations']]
-
-
-    # TODO: I am trying to clean up my messages in the terminal so I can elaborate an action plan on how to move forward.
-    # Thinking there might be an error on how I am managing the start and end dates. Perhaps should be adding an additional 1
-    # to the end date. Not sure if this is correct yet. Suppose I need 5 readings, because that is what Finex gave me. This is
-    # what my master is. The number of bitcoin exchange observations and whatever Bank of Canada tries to give me, I should be
-    # getting.
-
-
+    rates = fill_missing_day_rates(api_data)
+    return rates
 
 
 def call_fx_api(start_date, end_date):
@@ -70,31 +60,40 @@ def call_fx_api(start_date, end_date):
     return json.loads(response.text)
 
 
+def payload_is_not_empty(payload):
+    return (len(payload['observations']) != 0 )
+
+
+def strip_payload(payload):
+    return {i['d']: i['FXCADUSD']['v'] for i in payload['observations']}
+
+
 def fill_missing_day_rates(rates):
     # Bitcoin never closes its branch, so need to make up for banks missing rates
-    # data (i.e. holidays and weekends). A day is added to the end_date value
-    # because the API information retrieved from Bitfinex
+    # data (i.e. holidays and weekends).
+    # For weekdays, Bank of Canada will have a matching entry with corresponding rate.
+    # For this scenario, the data given is appended to result.
+    # When an entry is missing from the Bank of Canada response (holiday/weekend),
+    # the algorithm will use the last previous rate added to result.
+    # For edge case where the date range requested is only all holidays or weekends,
+    # a global FX rate is used (updated manually).
+    global AVG_FXCADUSD
+
     start_date = datetime.datetime.strptime(rates[0]['start_date'], '%Y-%m-%d')
     end_date = datetime.datetime.strptime(rates[0]['end_date'], '%Y-%m-%d')
-    requested_days = (end_date - start_date)
-    # print('(fill_missing_day rates) start_date:{}, end_date:{}'.format(start_date, end_date))
+    result = []
+    curr = start_date
+    result_has_data = False
 
-    new_end_date = end_date + datetime.timedelta(days=1)
-    print('new end date', new_end_date)
-    new_requested_days = new_end_date - start_date
-
-    print('API rate data len (days included in readings): {}, data:{},'
-          ' original_requested_days:{}, '
-          'new_requested_days:{}'.format(len(rates[1]),
-                                         rates[1],
-                                         requested_days,
-                                         new_requested_days))
-
-
-
-    # print(datetime.date(rates['start_date']) - datetime.date(rates['end_date']))
+    while True:
+        if curr.strftime('%Y-%m-%d') in rates[1].keys():
+            result.append(rates[1][curr.strftime('%Y-%m-%d')])
+            result_has_data = True
+        else:  # Duplicate last observation item because Bank of Canada has no data
+            result.append(result[-1]) if result_has_data else result.append(AVG_FXCADUSD)
+        curr = curr + datetime.timedelta(days=1)
+        if curr > end_date:
+            break
+    return result
 
 
-
-
-    sys.exit(1)
--- utests.py
+++ utests.py
@@ -10,10 +10,33 @@ class TestLoanClass (unittest.TestCase):
         resp = tools.get_cadusd_rates(start_date='2019-11-04', end_date='2019-11-08')
         self.assertEqual(len(resp), 5, 'Should be 5')
 
-    # def test_get_rates_holiday(self):
-    #     # Nov 11th is holiday
-    #     resp = tools.get_cadusd_rates(start_date='2019-11-11', end_date='2019-11-15')
-    #     self.assertEqual(len(resp), 5, 'Should be 5')
+    def test_get_rates_holiday_first_day(self):
+        # Nov 11th is holiday
+        resp = tools.get_cadusd_rates(start_date='2019-11-11', end_date='2019-11-14')
+        self.assertEqual(len(resp), 4, 'Should be 4')
+
+    def test_get_rates_only_holiday(self):
+        # Nov 11th is holiday
+        resp = tools.get_cadusd_rates(start_date='2019-11-11', end_date='2019-11-11')
+        self.assertEqual(len(resp), 1, 'Should be 1')
+        self.assertEqual(resp[0], tools.AVG_FXCADUSD, 'Should be equal to AVG_FXCADUSD')
+
+    def test_get_rates_weekend_between(self):
+        # Nov 11th is holiday
+        resp = tools.get_cadusd_rates(start_date='2019-10-28', end_date='2019-11-04')
+        self.assertEqual(len(resp), 8, 'Should be 8')
+
+    def test_get_rates_weekend_start(self):
+        resp = tools.get_cadusd_rates(start_date='2019-11-02', end_date='2019-11-09')
+        self.assertEqual(len(resp), 8, 'Should be 8')
+
+    def test_get_rates_weekend_end(self):
+        resp = tools.get_cadusd_rates(start_date='2019-11-08', end_date='2019-11-10')
+        self.assertEqual(len(resp), 3, 'Should be 3')
+
+    def test_get_rates_weekend_start(self):
+        resp = tools.get_cadusd_rates(start_date='2019-11-09', end_date='2019-11-10')
+        self.assertEqual(len(resp), 2, 'Should be 2')
 
 if __name__ == '__main__':
     unittest.main()
