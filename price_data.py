c3351ff858f8b1becf25547ca0d21e5142ca88a6
Daniel Olaya <d_olaya@hotmail.com>
2019-12-14 12:45:56 -0800

    PriceData; load_dataframe turned into static method

--- price_data.py
+++ price_data.py
@@ -4,17 +4,17 @@ from tools import update_btcusd_csv
 
 
 class PriceData:
-
     def __init__(self):
-        self.df_btcusd = self.load_price_dataframe()
+        self.df_btcusd = load_dataframe()
 
-    def load_price_dataframe(self):
-        update_btcusd_csv()
-        try:
-            self.df_btcusd = pd.read_csv('./data/btcusd.csv')
-        except FileNotFoundError:
-            print('[ERROR] Could not find file [/data/btcusd.csv].')
-            raise InitializationDataNotFound
-        self.df_btcusd['Date'] = pd.to_datetime(self.df_btcusd['Date'])
-        self.df_btcusd['Last'] = pd.to_numeric(self.df_btcusd['Close'])
-        return self.df_btcusd
+
+def load_dataframe():
+    update_btcusd_csv()
+    try:
+        df_btcusd = pd.read_csv('./data/btcusd.csv')
+    except FileNotFoundError:
+        print('[ERROR] Could not find file [/data/btcusd.csv].')
+        raise InitializationDataNotFound
+    df_btcusd['Date'] = pd.to_datetime(df_btcusd['Date'])
+    df_btcusd['Last'] = pd.to_numeric(df_btcusd['Close'])
+    return df_btcusd
