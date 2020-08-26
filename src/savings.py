7d20cac5dbcde22c77a0f65ba16ad4b348aeabf6
Daniel Olaya <daniel.olayam@gmail.com>
2020-08-26 16:19:16 -0700

    Daily rate added to Saving stats

--- src/savings.py
+++ src/savings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env/ python3
 
+import collections
 import pandas as pd
 import config as cfg
 from exceptions import InitializationDataNotFound, InvalidData
@@ -13,19 +14,17 @@ class Savings:
     rates_input_df = None
     stats = pd.DataFrame()
     # Todo: remove account from below
-    account_balance_history_btc = {}    # {date: +amount increased / -amount decreased}
+    account_balance_history_btc = collections.OrderedDict()    # {date: +amount increased / -amount decreased}
     balance_btc = 0
-    rate_daily_history = {}  # {date: daily interest rate}
-    rate_daily = 0
+    daily_rate_history = collections.OrderedDict()  # {date: daily interest rate}
+    daily_rate = 0
 
 
 def init_savings(rates_file='rates.csv'):
     Savings.account_input_df, Savings.rates_input_df = load_input_file(rates_file)
-    Savings.rate_daily = get_rates()
+    Savings.daily_rate = load_rates()
     get_total_savings_btc()
     calculate_stats()
-    # TODO: order savings_df by column date
-    # TODO: order interest_df by column date
 
 
 def load_input_file(rates_file):
@@ -47,12 +46,11 @@ def load_input_file(rates_file):
     return savings_input_df, rates_input_df
 
 
-def get_rates():
-    # TODO: make sure rates_input_df is always sorted from newest date to oldest
+def load_rates():
     daily_interest_rate = 0
     for index, row in Savings.rates_input_df.iterrows():
-        daily_interest_rate = round((row['apy']/365)/100, 5)
-        Savings.rate_daily_history.update({pd.Timestamp(row['date']): daily_interest_rate})
+        daily_interest_rate = round((row['apy']/365)/100, 6)
+        Savings.daily_rate_history.update({pd.Timestamp(row['date']): daily_interest_rate})
     return daily_interest_rate
 
 
@@ -65,25 +63,25 @@ def get_total_savings_btc():
         if row['type'] is cfg.DECREASE:
             Savings.balance_btc -= row['amount_btc']
             Savings.account_balance_history_btc.update({pd.Timestamp(row['date']): -row['amount_btc']})
-    # print(f'account_balance_history_btc:{Savings.account_balance_history_btc}')
 
 
 def calculate_stats():
     account_start_date = Savings.account_input_df["date"].iloc[0]
     Savings.stats['date'] = df_btcusd[df_btcusd['Date'] >= account_start_date]['Date']
-    # Savings.stats['rate_daily'] = calculate_daily_rates()
+    Savings.stats['daily_rate'] = calculate_daily_rates()
     # Savings.stats['balance_btc'] = populate_balances_btc()
 
 
-# def calculate_daily_rates():
-#     print(f'rate_daily_history: {Savings.rate_daily_history}')
-#     print(f'rate_daily:{Savings.rate_daily}')
-#     dates_with_rate_update = list(Savings.rate_daily_history.keys())
-#     curr_rate = Savings.rate_daily
-#     for index, row in Savings.stats.iterrrows():
-#         if row['date'] in dates_with_rate_update:
-#             print(f'date in list, {row["date"]}, {Savings.daily_history[row["date"]]}')
-
+def calculate_daily_rates():
+    daily_rate_df = []
+    dates_with_rate_update = list(Savings.daily_rate_history.keys())
+    first_rate_update = list(Savings.daily_rate_history.items())[0][1]
+    curr_rate = first_rate_update
+    for index, row in Savings.stats[::-1].iterrows():
+        if row['date'] in dates_with_rate_update:
+            curr_rate = Savings.daily_rate_history[row['date']]
+        daily_rate_df.append(curr_rate)
+    return daily_rate_df[::-1]
 
 # TODO: change below to dates_with_balance_update
 # def populate_balances_btc():
--- tests/test_savings.py
+++ tests/test_savings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections
 import unittest
 import sys
 import os
@@ -18,6 +19,10 @@ class TestSavings(unittest.TestCase):
     def setUp(self):
         # Resetting class variables between tests
         savings.Savings.balance_btc = 0
+        savings.Savings.daily_rate = 0
+        savings.Savings.account_balance_history_btc = collections.OrderedDict()
+        savings.Savings.daily_rate_history = collections.OrderedDict()
+
         # Show all rows/columns when pandas DF is printed
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', None)
@@ -34,7 +39,7 @@ class TestSavings(unittest.TestCase):
 
     def test_sort_input_df(self):
         cfg.set_test_mode('savings_1.csv')
-        savings.init_savings('rates_1.csv')
+        savings.init_savings('rates_0.csv')
         self.assertEqual(savings.Savings.rates_input_df.iloc[0]['date'], '2020-06-01', 'Incorrect sort order')
         self.assertEqual(savings.Savings.account_input_df.iloc[0]['date'], '2020-08-01', 'Incorrect sort order')
 
@@ -43,18 +48,28 @@ class TestSavings(unittest.TestCase):
         savings.init_savings('rates_0.csv')
         self.assertEqual(savings.Savings.balance_btc, 10.5, 'Should be 10.5')
 
+    def test_interest_rate_no_change(self):
+        cfg.set_test_mode('savings_3.csv')
+        savings.init_savings('rates_1.csv')
+        stats = savings.Savings.stats
+        # savings_df = savings.Savings.account_input_df
+        # rates_df = savings.Savings.rates_input_df
+        self.assertEqual(stats.iloc[0]['daily_rate'], 0.003288, 'Wrong daily interest rate')
 
-    # def test_interest_rate_no_change(self):
-    #     cfg.set_test_mode('savings_2.csv')
-    #     savings.init_savings('rates_0.csv')
-    #     savings_df = savings.Savings.account_input_df
-    #     rates_df = savings.Savings.rates_input_df
+    def test_interest_rate_change(self):
+        cfg.set_test_mode('savings_3.csv')
+        savings.init_savings('rates_2.csv')
+        stats = savings.Savings.stats
+        savings_df = savings.Savings.account_input_df
+        rates_df = savings.Savings.rates_input_df
+        self.assertEqual(stats[stats['date'] == '2020-08-01']['daily_rate'].values[0], 0.003288, 'Wrong daily rate')
+        self.assertEqual(stats[stats['date'] == '2020-08-10']['daily_rate'].values[0], 0.006575, 'Wrong daily rate')
 
         # print(savings.Savings.stats)
         # print(f'\n***Rates df***')
-        # print(savings.Savings.rates_input_df)
+        # print(rates_df)
         # print(f'\n***Savings df**')
-        # print(savings.Savings.account_input_df)
+        # print(savings_df)
 
 if __name__ == '__main__':
     unittest.main()
